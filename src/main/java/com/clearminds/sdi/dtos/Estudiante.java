f6b98afd86f5268b2713584128530acebbad3ac8
Sofía Díaz <soufito21@gmail.com>
2020-12-10 09:24:26 -0500

    Adición de la variable edad

--- src/main/java/com/clearminds/sdi/dtos/Estudiante.java
+++ src/main/java/com/clearminds/sdi/dtos/Estudiante.java
@@ -3,24 +3,41 @@ package com.clearminds.sdi.dtos;
 public class Estudiante {
 	private String nombre;
 	private String apellido;
+	private int edad;
 	private int id;
 	
-	public Estudiante(String nombre, String apellido, int id) {
+	
+	public Estudiante(String nombre, String apellido, int edad, int id) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 		this.id = id;
 	}
-	
+
 	public Estudiante(String nombre, String apellido) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 	}
 
+	public Estudiante(){
+		
+	}
+	
+	
+
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", id=" + id + "]";
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 
 	public String getNombre() {
